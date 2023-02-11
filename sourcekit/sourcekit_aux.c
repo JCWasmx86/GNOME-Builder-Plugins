417d76cc50e02cb5f93d688aa9de77ee3bfa9e54
JCWasmx86 <JCWasmx86@t-online.de>
2023-02-11 18:47:23 +0100

    Remove sourcekit plugin.
    It was added upstream a while ago

--- sourcekit/sourcekit_aux.c
+++
@@ -1,9 +0,0 @@
-#include "libide-lsp.h"
-
-extern GType sourcekit_service_get_type(void) G_GNUC_CONST;
-
-void bind_client(IdeObject *obj) {
-  g_autoptr(IdeLspServiceClass) clazz =
-      g_type_class_ref(sourcekit_service_get_type());
-  ide_lsp_service_class_bind_client(clazz, obj);
-}
